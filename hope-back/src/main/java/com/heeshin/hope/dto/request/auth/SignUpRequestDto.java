0e99336b28b34a84a03642615a76939bca03f762
Heechul Shin <heeshin174@gmail.com>
2025-04-20 07:11:53 +0900

    sign up dto

--- hope-back/src/main/java/com/heeshin/hope/dto/request/auth/SignUpRequestDto.java
+++ hope-back/src/main/java/com/heeshin/hope/dto/request/auth/SignUpRequestDto.java
@@ -1,4 +1,33 @@
 package com.heeshin.hope.dto.request.auth;
 
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class SignUpRequestDto {
+
+    @NotBlank @Email
+    private String email;
+
+    @NotBlank @Size(min=8, max=20)
+    private String password;
+    @NotBlank
+    private String nickname;
+
+    @NotBlank @Pattern(regexp = "^[0-9]{11,13}$")
+    private String telNumber;
+
+    @NotBlank
+    private String address;
+
+    private String addressDetail;
+
+    // true인 상태만 받
+    @NotNull @AssertTrue
+    private String agreedPersonal;
+
 }
--- hope-back/src/main/java/com/heeshin/hope/dto/response/auth/SignUpResponseDto.java
+++ hope-back/src/main/java/com/heeshin/hope/dto/response/auth/SignUpResponseDto.java
@@ -1,4 +1,34 @@
 package com.heeshin.hope.dto.response.auth;
 
-public class SignUpResponseDto {
+import com.heeshin.hope.common.ResponseCode;
+import com.heeshin.hope.common.ResponseMessage;
+import com.heeshin.hope.dto.ResponseDto;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+public class SignUpResponseDto extends ResponseDto {
+
+    private SignUpResponseDto() {
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+    }
+
+    public static ResponseEntity<SignUpResponseDto> success() {
+        SignUpResponseDto result = new SignUpResponseDto();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> duplicateEmail() {
+        ResponseDto result = new ResponseDto(ResponseCode.DUPLICATE_EMAIL, ResponseMessage.DUPLICATE_EMAIL);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+    public static ResponseEntity<ResponseDto> duplicateNickname() {
+        ResponseDto result = new ResponseDto(ResponseCode.DUPLICATE_NICKNAME, ResponseMessage.DUPLICATE_NICKNAME);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+    public static ResponseEntity<ResponseDto> duplicateTelNumber() {
+        ResponseDto result = new ResponseDto(ResponseCode.DUPLICATE_TEL_NUMBER, ResponseMessage.DUPLICATE_TEL_NUMBER);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
 }
