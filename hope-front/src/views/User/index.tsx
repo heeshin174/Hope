4bac841a15c9a221e01f4ef5442a865d79591772
Heechul Shin <heeshin174@gmail.com>
2025-04-29 19:13:54 +0900

    user page

--- hope-front/src/views/User/index.tsx
+++ hope-front/src/views/User/index.tsx
@@ -1,19 +1,28 @@
 import { ChangeEvent, useEffect, useRef, useState } from 'react'
 import './style.css'
 import defaultProfileImage from '@/assets/images/default-profile-image.jpg';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { BoardListItem } from 'types/interface';
+import BoardItem from 'components/BoardItem';
+import { useLoginUserStore } from 'stores';
+import { BOARD_PATH, BOARD_WRITE_PATH, USER_PATH } from 'constant';
 
 // component
 export default function User() {
 
 	// state: 
 	const { userEmail } = useParams();
+	// state: loginUser state
+	const { loginUser } = useLoginUserStore();
+	const [isMyPage, setMyPage] = useState<boolean>(false);
+
+	// function: navigate to board list
+	const navigate = useNavigate();
 
 	// component
 	const UserTop = () => {
 
 		// state
-		const [isMyPage, setMyPage] = useState<boolean>(true);
 		const [isNicknameChange, setIsNicknameChange] = useState<boolean>(false);
 		const [nickname, setNickname] = useState<string>('');
 		const [changeNickname, setChangeNickname] = useState<string>('');
@@ -90,9 +99,60 @@ export default function User() {
 	
 	// component
 	const UserBottom = () => {
+
+		// state
+		const [count, setCount] = useState<number>(0);
+		const [userBoardList, setBoardList] = useState<BoardListItem[]>([]);
+
+		// event handler: side card click event handler
+		const onSideCardClickHandler = () => {
+			if (isMyPage) navigate(BOARD_PATH() + '/' + BOARD_WRITE_PATH());
+			else if (loginUser) navigate(USER_PATH(loginUser.email));
+		};
+
+
+		// effect
+		useEffect(() => {
+
+		}, [userEmail]);
+
+		// render
 		return (
-			<div className="">
-	
+			<div className="user-bottom-wrapper">
+				<div className="user-bottom-container">
+					<div className="user-bottom-title">{isMyPage ? '내 게시물 ' : '게시물 ' }<span className="emphasis">{count}</span></div>
+					<div className="user-bottom-contents-box">
+					{count === 0 ? 
+					<div className="user-bottom-contents-nothing">{'게시물이 없습니다.'}</div> :
+					<div className="user-bottom-contents">
+						{userBoardList.map((boardListItem, index) => (
+							<BoardItem key={index} boardListItem={boardListItem} />
+						))}
+					</div>
+					}
+					<div className="user-bottom-side-box">
+						<div className="user-bottom-side-card" onClick={onSideCardClickHandler}>
+							<div className="user-bottom-side-container">
+								{isMyPage ? 
+								<>
+									<div className="icon-box">
+										<div className="icon edit-light-icon"></div>
+									</div>
+									<div className="user-bottom-side-text">{'글쓰기'}</div>
+								</> :
+								<>
+									<div className="user-bottom-side-text">{'내 게시물로 가기'}</div>
+									<div className="icon-box">
+										<div className="icon arrow-right-icon"></div>
+									</div>
+								</>
+								}
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="user-bottom-pagination-box"></div>
+				</div>
 			</div>
 		)
 	}
